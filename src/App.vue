2802f3243dc6cd3cce0e338ef0be669c23039a75
DanitoA1 <ahiabadaniel@gmail.com>
2021-01-10 20:39:12 +0100

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main
+      style="
+      background: rgb(200,8,95);
+      background: linear-gradient(90deg, rgba(200,8,95,1) 50%, rgba(221,10,125,1) 50%);
+      "
+    >
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,229 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row no-gutters>
+      <v-col cols="12" class="pos1">
+        <h1
+          class="monserrat text-center spaced white--text"
+        >
+          {{ datas.name }}
+        </h1>
+      </v-col>
+
+      <v-col class="pos2">
+        <v-card
+          class="mx-auto align-end"
+          color="rgba(221,10,125,1)"
+          elevation="24"
+          height="60vh"
+          width="50%"
+        >
+          <v-img
+            class="mx-auto img"
+            src="@/assets/aquila3.png"
+            height="46%"
+            width="38%"
+            contain
+          ></v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row
+      align="center"
+      justify="center"
+      class="mx-auto pos3"
+    >
+      <v-col
+        class="text-center"
+      >
+        <v-btn
+          class="font-weight-light"
+          dark
+          text
+        >
+          HOME
+        </v-btn>
+      </v-col>
+      <v-col
+        class="text-center"
+      >
+        <v-btn
+          class="font-weight-light"
+          dark
+          text
+        >
+          ABOUT
+        </v-btn>
+      </v-col>
+      <v-col>
+        <v-row
+          class="font-weight-light"
+          justify="center"
+        >
+          <v-avatar
+            size="78"
+          >
+            <img
+              src="@/assets/aquila.jpg"
+              alt="John"
+            >
+          </v-avatar>
+        </v-row>
+      </v-col>
+      <v-col
+        class="text-center"
+      >
+        <v-btn
+          class="font-weight-light"
+          dark
+          text
+        >
+          GALLERY
+        </v-btn>
+      </v-col>
+      <v-col
+        class="text-center"
+      >
+        <v-btn
+          class="font-weight-light"
+          dark
+          text
+        >
+          CONTACT
+        </v-btn>
+      </v-col>
+    </v-row>
+    <div
+      class="pos4"
+    >
+      <v-btn
+        icon
+        class="mb-4"
+        :href="instagramLink"
+      >
+        <v-icon
+          color="white"
+        >
+          mdi-instagram
+        </v-icon>
+      </v-btn><br>
+      <v-btn
+        icon
+        class="mb-4"
+        :href="twitterLink"
+      >
+        <v-icon
+          color="white"
+        >
+          mdi-twitter
+        </v-icon>
+      </v-btn><br>
+      <v-btn
+        icon
+        class="mb-4"
+        :href="snapchatLink"
+      >
+        <v-icon
+          color="white"
+        >
+          mdi-snapchat
+        </v-icon>
+      </v-btn><br>
+      <v-btn
+        icon
+        class="mb-4"
+        :href="emailLink"
+      >
+        <v-icon
+          color="white"
+        >
+          mdi-email
+        </v-icon>
+      </v-btn>
+   </div>
+ 
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  import axios from 'axios';
+
+  export default {
+    name: 'HelloWorld',
+    mounted () {
+      axios.get("https://hirng-x2021.glitch.me/api" , {
+          headers: {}
+      })
+      .then(response => {
+          this.datas = response.data
+          console.log(response.data);
+      })
+      .catch(function (error) {
+            console.log(error);
+      });
+    },
+    created () {
+      this.linkAm();
+    },
+
+    data: () => ({
+      datas: '',
+    }),
+    computed: {
+      instagramLink () {
+        return `https://instagram.com/${this.datas.social_media.instagram}`;
+      },
+      twitterLink () {
+        return `https://twitter.com/${this.datas.social_media.twitter}`;
+      },
+      snapchatLink () {
+        return `https://snapchat.com/${this.datas.social_media.snapchat}`;
+      },
+      emailLink () {
+        return `mailto:${this.datas.social_media.email}`
+      }
+    }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300&display=swap');
+  .monserrat {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 100;
+    font-size: 40px;
+  }
+  .spaced {
+    letter-spacing: 8px;
+  }
+  .pos1 {
+    position: fixed;
+    left: 0%;
+    right: 0%;
+    top: 6%;
+  }
+  .pos2 {
+    position: fixed;
+    left: 0%;
+    right: 0%;
+    top: 20%;
+  }
+  .pos3 {
+    position: fixed;
+    top: 86%;
+    left: 0%;
+    right: 0%;
+    max-width: 70%;
+  }
+  .pos4 {
+    position: fixed;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 3%;
+  }
+  .img {
+    position: fixed;
+    left: 0%;
+    right: 0%;
+    top: 35.2%;
+  }
 </style>
